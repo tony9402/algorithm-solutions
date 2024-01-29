bf4ec4038e9477c8f5ca61751207fb2ac9650c6c
tony9402 <tony9402@naver.com>
2024-01-30 04:58:49 +0900

    1001 CPP solution

---
+++ solutions/baekjoon/1001/main.cpp
@@ -0,0 +1,19 @@
+// Authored by: tony9402
+// Co-authored by: -
+// Link: http://boj.kr/0f9b0c569a144394a24ff3e7df3a2fae
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int a, b; cin >> a >> b;
+    cout << a - b;
+    return 0;
+}
+
+/* Solution Description
+두 정수 A와 B를 입력받고 두 정수의 차의 결과를 출력하면 되는 문제
+*/
