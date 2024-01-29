05daeb4e01aad8cd55e534ac19ff73a7358f67b2
tony9402 <tony9402@naver.com>
2024-01-30 05:03:52 +0900

    1001 Python Solution

---
+++ solutions/baekjoon/1001/main.py
@@ -0,0 +1,14 @@
+# Authored by: tony9402
+# Co-authored by: -
+# Link: http://boj.kr/7a8867fd7d4141f09eb1ced81591d4b0
+import sys
+
+def input():
+    return sys.stdin.readline().rstrip()
+
+a, b = map(int, input().split())
+print(a - b)
+
+""" Solution Description
+두 정수 A와 B를 입력받고 두 정수의 차의 결과를 출력하면 되는 문제
+"""
