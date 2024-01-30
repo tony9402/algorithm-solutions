9aa56df8782945a1702b32dbda7bba0f323d5373
lms0806 <godmlzkf1@naver.com>
2024-01-30 20:08:38 +0900

    update tab to space

--- solutions/baekjoon/1001/Main.java
+++ solutions/baekjoon/1001/Main.java
@@ -1,25 +1,21 @@
 // Authored by: lms0806
 // Co-authored by: -
 // Link: http://boj.kr/fbea125ef3714da4b712fdbd4415d43f
-import java.util.*;
-import java.io.*;
-import java.lang.*;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-		int a = Integer.parseInt(st.nextToken());
-		int b = Integer.parseInt(st.nextToken());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
 
-		System.out.print(a - b);
-	}
+        System.out.print(a - b);
+    }
 }
 
 /* Solution Description
