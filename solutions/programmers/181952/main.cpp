3dac3af16df4e0786dbe047c20599905b84841b4
tony9402 <tony9402@naver.com>
2024-02-02 01:53:28 +0900

    [ADD] programmers 181952 C++ solution

---
+++ solutions/programmers/181952/main.cpp
@@ -0,0 +1,17 @@
+// Authored by: tony9402
+// Co-authored by: -
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    string str;
+    cin >> str;
+    cout << str;
+    return 0;
+}
+
+/* Solution Description
+주어진 문자열을 그대로 출력하면 되는 문제
+*/
