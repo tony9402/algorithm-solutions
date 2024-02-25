c7f1fb01c5c23abeac6740f1d39c6df248528e77
Minsang Kim <tony9402@naver.com>
2024-02-25 22:08:48 +0900

    refactoring

--- solutions/baekjoon/17142/main.py
+++ solutions/baekjoon/17142/main.py
@@ -1,8 +1,7 @@
 # Authored by : 220v
-# Co-authored by : -
-# Link : https://www.acmicpc.net/source/61069486
+# Co-authored by : tony9402
+# Link : http://boj.kr/d80c341225ce4988805cca9a6a201a84
 import sys
-import copy
 from collections import deque
 from itertools import combinations
 
@@ -11,17 +10,18 @@ def input():
 
 # input
 N, M = map(int, input().split())
-lab = []
-for _ in range(N):
-    lab.append(list(map(int, input().split())))
+lab = [list(map(int, input().split())) for _ in range(N)]
 
 # delta row, delta col.
-dr = [1, 0, -1, 0]
-dc = [0, 1, 0, -1]
+dr = (1, 0, -1, 0)
+dc = (0, 1, 0, -1)
 
-# find 2 in laboratory / count 0 in lab / change numbers(wall, virus)
+# save Minimum Second to infect all blocks.
+INF = 1000000
+minSec = INF
+
+# find 2 in laboratory / change numbers(wall, virus)
 twos = []
-cnt_0 = 0
 for r, row in enumerate(lab):
     for c, i in enumerate(row):
         if i == 2:
@@ -29,31 +29,9 @@ for r, row in enumerate(lab):
             lab[r][c] = "*"
         elif i == 1:
             lab[r][c] = "-"
-        elif i == 0:
-            cnt_0 += 1
 
-# Combination - (len of twos) C (M) - find all combination of index.
-start = list(combinations(twos, M))
-
-# save Minimum Second to infect all blocks.
-minSec = 1000000
-
-def isAllInfected(inLab):
-    # function to know all blocks infected.
-    for row in inLab:
-        for c in row:
-            if c == 0:
-                return False
-
-    return True
-
-# check if minSec = 0
-if isAllInfected(lab):
-    print(0)
-    exit()
 
 def check(inLab):
-    count0 = 0  # 0의 갯수 확인
     maxCount = 0  # 최단 거리 계산
     for two in twos:
         inLab[two[0]][two[1]] = "*"
@@ -61,52 +39,49 @@ def check(inLab):
     for i in range(N):
         for j in range(N):
             if inLab[i][j] == 0:
-                return -1
+                return INF
 
             if inLab[i][j] != '*' and inLab[i][j] != '-':
                 maxCount = max(maxCount, inLab[i][j])
 
-    if count0 == 0:
-        return maxCount
+    return maxCount
+
 
 # cycle of combinations
-for s in start:
+# Combination - (len of twos) C (M) - find all combination of index.
+for s in combinations(twos, M):
     sec = 0
     q = deque(s)
-    templab = copy.deepcopy(lab)
+    templab = [row[:] for row in lab]
 
     # BFS - one cycle (1 case)
     while q:
         sec += 1
-        tempq = deque()
+        cntq = len(q)
 
         # BFS - one cycle (1 sec)
-        while q:
+        while cntq > 0:
             r, c = q.popleft()
+            cntq -= 1
 
             for i in range(4):
-                nr = r + dr[i]
-                nc = c + dc[i]
+                nr, nc = r + dr[i], c + dc[i]
                 # check if wall / if virus already infected.
-                if N > nr >= 0 and N > nc >= 0 and (templab[nr][nc] == 0 or templab[nr][nc] == "*"):
-                    tempq.append([nr, nc])
+                if 0 <= nr < N and 0 <= nc < N and (templab[nr][nc] == 0 or templab[nr][nc] == "*"):
+                    q.append([nr, nc])
                     # save sec for infect this block.
                     templab[nr][nc] = sec
 
-        q = tempq
+    minSec = min(minSec, check(templab))
 
-    a = check(templab)
-    if a != -1 and minSec > a:
-        minSec = a
-
-if minSec != 1000000:
-    print(minSec)
-else:
+if minSec == INF:
     print(-1)
+else:
+    print(minSec)
 
 """ Solution Description
-'비활성화된 바이러스 또한 전염이 진행될 수 있도록 처리할 것.'
-'비활성화된 바이러스가 활성화되는 것은 최소 시간을 측정하는 데에 고려하지 않아야 할 것.'
-위 두 가지 조건과, 시간 초과가 나지 않도록 신경쓰며 BFS를 이용해 문제를 풀이한다.
-https://wondev.tistory.com/157
+'비활성화된 바이러스 또한 전염이 진행될 수 있도록 처리할 것.'  
+'비활성화된 바이러스가 활성화되는 것은 최소 시간을 측정하는 데에 고려하지 않아야 할 것.'  
+위 두 가지 조건과, 시간 초과가 나지 않도록 신경쓰며 BFS를 이용해 문제를 풀이한다.  
+https://wondev.tistory.com/157  
 """
