f66f30d49c9a3eb3bec20fa5baa408b8025985c7
Minsang Kim <tony9402@naver.com>
2025-01-29 15:55:00 +0900

    Refactoring

--- solutions/baekjoon/1229/Main.java
+++ solutions/baekjoon/1229/Main.java
@@ -6,74 +6,74 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	public static void main(String[] args) {
-		FastReader rd = new FastReader();
-		int N = rd.nextInt();
+    public static void main(String[] args) {
+        FastReader rd = new FastReader();
+        int N = rd.nextInt();
 
-		List<Integer> list = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
 
-		// 육각수 만들기
-		list.add(1);
-		int sum = 5;
-		while (list.get(list.size() - 1) + sum <= N) {
-			list.add(list.get(list.size() - 1) + sum);
-			sum += 4;
-		}
+        // 육각수 만들기
+        list.add(1);
+        int sum = 5;
+        while (list.get(list.size() - 1) + sum <= N) {
+            list.add(list.get(list.size() - 1) + sum);
+            sum += 4;
+        }
 
 
-		int dp[] = new int[N + 1];
-		for (int i = 1; i <= N; i++) {
-			dp[i] = 6;
-			for (int s : list) {
-				if (s > i) break;
-				dp[i] = Math.min(dp[i], dp[i - s] + 1);
-			}
-		}
+        int dp[] = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            dp[i] = 6;
+            for (int s : list) {
+                if (s > i) break;
+                dp[i] = Math.min(dp[i], dp[i - s] + 1);
+            }
+        }
 
-		System.out.println(dp[N]);
-	}
+        System.out.println(dp[N]);
+    }
 
-	static class FastReader {
-		BufferedReader br;
-		StringTokenizer st;
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
 
-		public FastReader() {
-			br = new BufferedReader(new InputStreamReader(System.in));
-		}
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
 
-		String next() {
-			while (st == null || !st.hasMoreElements()) {
-				try {
-					st = new StringTokenizer(br.readLine());
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-			return st.nextToken();
-		}
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
 
-		int nextInt() {
-			return Integer.parseInt(next());
-		}
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
 
-		long nextLong() {
-			return Long.parseLong(next());
-		}
+        long nextLong() {
+            return Long.parseLong(next());
+        }
 
-		double nextDouble() {
-			return Double.parseDouble(next());
-		}
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
 
-		String nextLine() {
-			String str = "";
-			try {
-				str = br.readLine();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			return str;
-		}
-	}
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
 }
 
 /* Solution Description
