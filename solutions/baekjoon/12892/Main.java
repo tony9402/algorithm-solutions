33102a6ec50a77bc3f7eba492b93e91b0dac4cd8
Minsang Kim <tony9402@naver.com>
2025-01-29 15:43:36 +0900

    Refactoring before merge

--- solutions/baekjoon/12892/Main.java
+++ solutions/baekjoon/12892/Main.java
@@ -1,88 +1,88 @@
 // Authored by: beberiche
-// Co-authored by: http://boj.kr/1e4c33777da24f94981b6b6062e2fbb6
-// Link:
+// Co-authored by: 
+// Link: http://boj.kr/1e4c33777da24f94981b6b6062e2fbb6
 
 import java.util.*;
 import java.io.*;
 
 public class Main {
-	public static void main(String[] args) {
-		FastReader rd = new FastReader();
+    public static void main(String[] args) {
+        FastReader rd = new FastReader();
 
-		int N = rd.nextInt();
-		int D = rd.nextInt();
-		int a[][] = new int[N][2];
+        int N = rd.nextInt();
+        int D = rd.nextInt();
+        int a[][] = new int[N][2];
 
 
-		for (int i = 0; i < N; i++) {
-			a[i][0] = rd.nextInt();
-			a[i][1] = rd.nextInt();
-		}
+        for (int i = 0; i < N; i++) {
+            a[i][0] = rd.nextInt();
+            a[i][1] = rd.nextInt();
+        }
 
-		Arrays.sort(a, (n1, n2) -> n1[0] - n2[0]);
+        Arrays.sort(a, (n1, n2) -> n1[0] - n2[0]);
 
-		int st = 0;
-		int ed = 0;
-		long ans = 0;
-		long curr = 0;
+        int st = 0;
+        int ed = 0;
+        long ans = 0;
+        long curr = 0;
 
-		while (ed < N) {
-			int diff = a[ed][0] - a[st][0];
+        while (ed < N) {
+            int diff = a[ed][0] - a[st][0];
 
-			if (diff < D) {
-				curr += a[ed][1];
-				ans = Math.max(ans, curr);
-				ed++;
-			} else {
-				curr -= a[st][1];
-				st++;
-			}
-		}
+            if (diff < D) {
+                curr += a[ed][1];
+                ans = Math.max(ans, curr);
+                ed++;
+            } else {
+                curr -= a[st][1];
+                st++;
+            }
+        }
 
-		System.out.println(ans);
-	}
+        System.out.println(ans);
+    }
 
-	static class FastReader {
-		BufferedReader br;
-		StringTokenizer st;
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
 
-		public FastReader() {
-			br = new BufferedReader(new InputStreamReader(System.in));
-		}
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
 
-		String next() {
-			while (st == null || !st.hasMoreElements()) {
-				try {
-					st = new StringTokenizer(br.readLine());
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-			return st.nextToken();
-		}
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
 
-		int nextInt() {
-			return Integer.parseInt(next());
-		}
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
 
-		long nextLong() {
-			return Long.parseLong(next());
-		}
+        long nextLong() {
+            return Long.parseLong(next());
+        }
 
-		double nextDouble() {
-			return Double.parseDouble(next());
-		}
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
 
-		String nextLine() {
-			String str = "";
-			try {
-				str = br.readLine();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			return str;
-		}
-	}
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
 }
 
 /* Solution Description
