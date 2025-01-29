d159b97998206dea15ad7accbbefde5875ad92e6
Minsang Kim <tony9402@naver.com>
2025-01-29 15:54:05 +0900

    Refactoring

--- solutions/baekjoon/1263/Main.java
+++ solutions/baekjoon/1263/Main.java
@@ -6,69 +6,69 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	public static void main(String[] args) {
-		FastReader rd = new FastReader();
-		int N = rd.nextInt();
-		int[][] a = new int[N][2];
+    public static void main(String[] args) {
+        FastReader rd = new FastReader();
+        int N = rd.nextInt();
+        int[][] a = new int[N][2];
 
-		for (int i = 0; i < N; i++) {
-			a[i][0] = rd.nextInt();
-			a[i][1] = rd.nextInt();
-		}
+        for (int i = 0; i < N; i++) {
+            a[i][0] = rd.nextInt();
+            a[i][1] = rd.nextInt();
+        }
 
-		Arrays.sort(a, (n1, n2) -> n2[1] - n1[1]);
+        Arrays.sort(a, (n1, n2) -> n2[1] - n1[1]);
 
 
-		int ans = a[0][1] - a[0][0];
-		for (int i = 1; i < N; i++) {
-			if (a[i][1] < ans) ans = a[i][1];
-			ans -= a[i][0];
-		}
+        int ans = a[0][1] - a[0][0];
+        for (int i = 1; i < N; i++) {
+            if (a[i][1] < ans) ans = a[i][1];
+            ans -= a[i][0];
+        }
 
-		System.out.println((ans < 0) ? -1 : ans);
-	}
+        System.out.println((ans < 0) ? -1 : ans);
+    }
 
-	static class FastReader {
-		BufferedReader br;
-		StringTokenizer st;
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
 
-		public FastReader() {
-			br = new BufferedReader(new InputStreamReader(System.in));
-		}
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
 
-		String next() {
-			while (st == null || !st.hasMoreElements()) {
-				try {
-					st = new StringTokenizer(br.readLine());
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-			return st.nextToken();
-		}
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
 
-		int nextInt() {
-			return Integer.parseInt(next());
-		}
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
 
-		long nextLong() {
-			return Long.parseLong(next());
-		}
+        long nextLong() {
+            return Long.parseLong(next());
+        }
 
-		double nextDouble() {
-			return Double.parseDouble(next());
-		}
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
 
-		String nextLine() {
-			String str = "";
-			try {
-				str = br.readLine();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			return str;
-		}
-	}
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
 }
 
 /* Solution Description
@@ -86,3 +86,4 @@ public class Main {
     b. 오름차순으로 정렬하여, 매개변수 탐색을 통해 정답을 구하는 것도 가능하다.
 
  */
+
