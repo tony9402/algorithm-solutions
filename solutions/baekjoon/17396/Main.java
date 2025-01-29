ec682d9934b80ab2cbd51b8aaeef8cb344e735cf
Minsang Kim <tony9402@naver.com>
2025-01-29 16:38:49 +0900

    Refactoring

--- solutions/baekjoon/17396/Main.java
+++ solutions/baekjoon/17396/Main.java
@@ -6,121 +6,121 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	public static void main(String[] args) {
-		FastReader rd = new FastReader();
-		int N = rd.nextInt();
-		int M = rd.nextInt();
+    public static void main(String[] args) {
+        FastReader rd = new FastReader();
+        int N = rd.nextInt();
+        int M = rd.nextInt();
 
-		int[] a = new int[N];
-		for (int i = 0; i < N; i++) {
-			a[i] = rd.nextInt();
-		}
+        int[] a = new int[N];
+        for (int i = 0; i < N; i++) {
+            a[i] = rd.nextInt();
+        }
 
-		List<Node> list[] = new ArrayList[N];
-		for (int i = 0; i < N; i++) {
-			list[i] = new ArrayList<>();
-		}
+        List<Node> list[] = new ArrayList[N];
+        for (int i = 0; i < N; i++) {
+            list[i] = new ArrayList<>();
+        }
 
-		for (int i = 0; i < M; i++) {
-			int n1 = rd.nextInt();
-			int n2 = rd.nextInt();
-			int dist = rd.nextInt();
+        for (int i = 0; i < M; i++) {
+            int n1 = rd.nextInt();
+            int n2 = rd.nextInt();
+            int dist = rd.nextInt();
 
-			boolean check1 = check(n1, a);
-			boolean check2 = check(n2, a);
+            boolean check1 = check(n1, a);
+            boolean check2 = check(n2, a);
 
-			if (check1 && check2) {
-				list[n1].add(new Node(n2, dist));
-				list[n2].add(new Node(n1, dist));
-			}
-		}
+            if (check1 && check2) {
+                list[n1].add(new Node(n2, dist));
+                list[n2].add(new Node(n1, dist));
+            }
+        }
 
-		long[] dist = new long[N];
-		long INF = (long) 3e11 + 4;
-		Arrays.fill(dist, INF);
-		dist[0] = 0;
+        long[] dist = new long[N];
+        long INF = (long) 3e11 + 4;
+        Arrays.fill(dist, INF);
+        dist[0] = 0;
 
-		PriorityQueue<Node> pq = new PriorityQueue<>();
-		pq.add(new Node(0, 0));
-		while (!pq.isEmpty()) {
-			Node curr = pq.poll();
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.add(new Node(0, 0));
+        while (!pq.isEmpty()) {
+            Node curr = pq.poll();
 
-			if (dist[curr.id] < curr.dist) continue;
+            if (dist[curr.id] < curr.dist) continue;
 
-			for (Node next : list[curr.id]) {
-				if (dist[next.id] > curr.dist + next.dist) {
-					dist[next.id] = curr.dist + next.dist;
-					pq.add(new Node(next.id, dist[next.id]));
-				}
-			}
-		}
+            for (Node next : list[curr.id]) {
+                if (dist[next.id] > curr.dist + next.dist) {
+                    dist[next.id] = curr.dist + next.dist;
+                    pq.add(new Node(next.id, dist[next.id]));
+                }
+            }
+        }
 
-//		System.out.println(-1);
-		long ans = dist[N - 1] == INF ? -1 : dist[N - 1];
-		System.out.println(ans);
-	}
+        //		System.out.println(-1);
+        long ans = dist[N - 1] == INF ? -1 : dist[N - 1];
+        System.out.println(ans);
+    }
 
-	private static boolean check(int node, int[] a) {
-		return node == a.length - 1 || a[node] == 0;
-	}
+    private static boolean check(int node, int[] a) {
+        return node == a.length - 1 || a[node] == 0;
+    }
 
-	private static class Node implements Comparable<Node> {
-		int id;
-		long dist;
+    private static class Node implements Comparable<Node> {
+        int id;
+        long dist;
 
 
-		Node(int id, long dist) {
-			this.id = id;
-			this.dist = dist;
-		}
+        Node(int id, long dist) {
+            this.id = id;
+            this.dist = dist;
+        }
 
-		@Override
-		public int compareTo(Node o) {
-			return Long.compare(this.dist, o.dist);
-		}
-	}
+        @Override
+        public int compareTo(Node o) {
+            return Long.compare(this.dist, o.dist);
+        }
+    }
 
-	static class FastReader {
-		BufferedReader br;
-		StringTokenizer st;
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
 
-		public FastReader() {
-			br = new BufferedReader(new InputStreamReader(System.in));
-		}
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
 
-		String next() {
-			while (st == null || !st.hasMoreElements()) {
-				try {
-					st = new StringTokenizer(br.readLine());
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-			return st.nextToken();
-		}
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
 
-		int nextInt() {
-			return Integer.parseInt(next());
-		}
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
 
-		long nextLong() {
-			return Long.parseLong(next());
-		}
+        long nextLong() {
+            return Long.parseLong(next());
+        }
 
-		double nextDouble() {
-			return Double.parseDouble(next());
-		}
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
 
-		String nextLine() {
-			String str = "";
-			try {
-				str = br.readLine();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			return str;
-		}
-	}
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
 }
 
 /* Solution Description
