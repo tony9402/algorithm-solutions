66104a0da53181c5035bd82ead717287bf7e1905
Minsang Kim <tony9402@naver.com>
2025-01-29 16:43:15 +0900

    Refactoring

--- solutions/baekjoon/2479/Main.java
+++ solutions/baekjoon/2479/Main.java
@@ -6,108 +6,105 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	public static void main(String[] args) {
-		FastReader rd = new FastReader();
-		int N = rd.nextInt();
-		int K = rd.nextInt();
+    public static void main(String[] args) {
+        FastReader rd = new FastReader();
+        int N = rd.nextInt();
+        int K = rd.nextInt();
 
-		int[] a = new int[N + 1];
-		for (int i = 1; i <= N; i++) {
-			int num = 1;
-			String str = rd.next();
-			int total = 0;
-			for (int j = K - 1; j >= 0; j--) { // 이진코드 -> int화
-				int val = str.charAt(j) - '0';
-				if (val == 1) {
-					total += val * num;
-				}
-				num *= 2;
-			}
-			a[i] = total;
-		}
+        int[] a = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            int num = 1;
+            String str = rd.next();
+            int total = 0;
+            for (int j = K - 1; j >= 0; j--) { // 이진코드 -> int화
+                int val = str.charAt(j) - '0';
+                if (val == 1) {
+                    total += val * num;
+                }
+                num *= 2;
+            }
+            a[i] = total;
+        }
 
+        int A = rd.nextInt();
+        int B = rd.nextInt();
+        Queue<Node> q = new LinkedList<>();
+        q.add(new Node(a[A], A+""));
+        boolean visited[] = new boolean[N + 1];
+        visited[A] = true;
 
-		int A = rd.nextInt();
-		int B = rd.nextInt();
-		Queue<Node> q = new LinkedList<>();
-		q.add(new Node(a[A], A+""));
-		boolean visited[] = new boolean[N + 1];
-		visited[A] = true;
+        String ans = "-1";
+        while (!q.isEmpty()) {
+            Node curr = q.poll();
+            if (curr.num == a[B]) {
+                ans = curr.path;
+                break;
+            }
 
-		String ans = "-1";
-		while (!q.isEmpty()) {
-			Node curr = q.poll();
-			if (curr.num == a[B]) {
-				ans = curr.path;
-				break;
-			}
+            for (int i = 1; i <= N; i++) {
+                if (visited[i] || a[i] == curr.num) continue;
 
-			for (int i = 1; i <= N; i++) {
-				if (visited[i] || a[i] == curr.num) continue;
+                if (Integer.bitCount(curr.num ^ a[i]) == 1) {
+                    visited[i] = true;
+                    q.add(new Node(a[i], curr.path + " " + i));
+                }
+            }
+        }
 
-				if (Integer.bitCount(curr.num ^ a[i]) == 1) {
-					visited[i] = true;
-					q.add(new Node(a[i], curr.path + " " + i));
-				}
-			}
-		}
+        System.out.println(ans);
+    }
 
-		System.out.println(ans);
-	}
+    private static class Node {
+        int num;
+        String path;
 
-	private static class Node {
-		int num;
-		String path;
+        Node(int num, String path) {
+            this.num = num;
+            this.path = path;
+        }
+    }
 
-		Node(int num, String path) {
-			this.num = num;
-			this.path = path;
-		}
-	}
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
 
-	static class FastReader {
-		BufferedReader br;
-		StringTokenizer st;
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
 
-		public FastReader() {
-			br = new BufferedReader(new InputStreamReader(System.in));
-		}
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
 
-		String next() {
-			while (st == null || !st.hasMoreElements()) {
-				try {
-					st = new StringTokenizer(br.readLine());
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-			return st.nextToken();
-		}
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
 
-		int nextInt() {
-			return Integer.parseInt(next());
-		}
+        long nextLong() {
+            return Long.parseLong(next());
+        }
 
-		long nextLong() {
-			return Long.parseLong(next());
-		}
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
 
-		double nextDouble() {
-			return Double.parseDouble(next());
-		}
-
-		String nextLine() {
-			String str = "";
-			try {
-				str = br.readLine();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			return str;
-		}
-	}
-
-
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
 }
 
 /* Solution Description
